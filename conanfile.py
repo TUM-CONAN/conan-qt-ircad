bebdb0d0a85ab24c975bf2db0ca4f5b1b7e3d66c
Ulrich Eck <ulrich.eck@tum.de>
2019-07-16 16:40:30 +0200

    build for ubuntu adapted to camposs

--- conanfile.py
+++ conanfile.py
@@ -28,23 +28,23 @@ class QtConan(ConanFile):
 
     def configure(self):
         del self.settings.compiler.libcxx
-        if 'CI' not in os.environ:
-            os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
+        # if 'CI' not in os.environ:
+        #     os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
 
     def requirements(self):
-        self.requires("common/1.0.0@sight/stable")
+        self.requires("ircad_common/1.0.0@camposs/stable")
         if tools.os_info.is_windows:
-            self.requires("zlib/1.2.11-r2@sight/stable")
-            self.requires("openssl/1.1.1b-r1@sight/stable")
+            self.requires("zlib/1.2.11@camposs/stable")
+            self.requires("openssl/1.1.1b@bincrafters/stable")
 
         if not tools.os_info.is_linux:
-            self.requires("libpng/1.6.34-r2@sight/stable")
-            self.requires("libjpeg/9c-r2@sight/stable")
-            self.requires("freetype/2.9.1-r2@sight/stable")
+            self.requires("libpng/1.6.36@bincrafters/stable")
+            self.requires("libjpeg/9c-r2@camposs/stable")
+            self.requires("freetype/2.9.1@bincrafters/stable")
 
     def build_requirements(self):
         if tools.os_info.is_windows:
-            self.build_requires("jom/1.1.2-r1@sight/stable")
+            self.build_requires("jom/1.1.2-r1@sight/stable") # not yet available ..
 
         if tools.os_info.linux_distro == "linuxmint":
             pack_names = [
@@ -92,6 +92,44 @@ class QtConan(ConanFile):
             for p in pack_names:
                 installer.install(p)
 
+        if tools.os_info.linux_distro == "ubuntu":
+            pack_names = [
+                'libfontconfig1-dev',
+                'libfreetype6-dev',
+                'libx11-dev',
+                'libxext-dev',
+                'libxfixes-dev',
+                'libxi-dev',
+                'libxrender-dev',
+                'libxcomposite-dev',
+                'libxcursor-dev',
+                'libxtst-dev',
+                'libxkbcommon-dev',
+                'libxkbcommon-x11-dev',
+                'libxcb1-dev',
+                'libx11-xcb-dev',
+                'libxcb-glx0-dev',
+                'libxcb-keysyms1-dev',
+                'libxcb-image0-dev',
+                'libxcb-shm0-dev',
+                'libxcb-icccm4-dev',
+                'libxcb-sync-dev',
+                'libxcb-xfixes0-dev',
+                'libxcb-shape0-dev',
+                'libxcb-randr0-dev',
+                'libxcb-render-util0-dev',
+                'libc6-dev',
+                'libgstreamer1.0-dev',
+                'libgstreamer-plugins-base1.0-dev',
+                'libjpeg-turbo8-dev',
+                'libssl-dev',
+                'openssl',
+                'libpng-dev',
+            ]
+            installer = tools.SystemPackageTool()
+            for p in pack_names:
+                installer.install(p)
+
     def system_requirements(self):
         if tools.os_info.linux_distro == "linuxmint":
             pack_names = [
@@ -122,6 +160,30 @@ class QtConan(ConanFile):
                     'libpng16-16'
                 ]
 
+            installer = tools.SystemPackageTool()
+            for p in pack_names:
+                installer.install(p)
+
+        if tools.os_info.linux_distro == "ubuntu":
+            pack_names = [
+                'libfontconfig1',
+                'libfreetype6',
+                'libxcb1',
+                'libxcb-glx0',
+                'libxcb-keysyms1',
+                'libxcb-image0',
+                'libxcb-shm0',
+                'libxcb-icccm4',
+                'libxcb-xfixes0',
+                'libxcb-shape0',
+                'libxcb-randr0',
+                'libxcb-render-util0',
+                'libgstreamer1.0-0',
+                'libgstreamer-plugins-base1.0-0',
+                'libjpeg-turbo8',
+                'openssl',
+                'libpng16-16',
+            ]
             installer = tools.SystemPackageTool()
             for p in pack_names:
                 installer.install(p)
